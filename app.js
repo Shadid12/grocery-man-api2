576b860da06e7683cd5896f1d400f4c571b31673
shadid12 <shadidhaque2012@gmail.com>
2018-07-03 15:06:24 -0400

    added new mongo jobs

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+mongoose.connect('mongodb://root2:shadid123456@ds042607.mlab.com:42607/shadid')
+    .then(() => {
+        console.log('DB is connected!!');
+    })
+  
 
 const itemRoutes = require('./api/routes/items');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 app.use('/items', itemRoutes);
 
