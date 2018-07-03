ee2f7d713af9af7344a403dc77e16656f0d35f19
shadid12 <shadidhaque2012@gmail.com>
2018-07-03 14:52:07 -0400

    added new morgan

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 
 const itemRoutes = require('./api/routes/items');
 
+app.use(morgan('dev'));
+
 app.use('/items', itemRoutes);
 
-
 app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works!'
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
     });
 });
 
